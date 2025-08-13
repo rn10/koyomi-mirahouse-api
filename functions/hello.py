275f955f1e3c6d5f20b2ca2c05a81ba2ee4c12ab
内藤亮介 <n10@glenlivet.local>
2025-08-13 16:49:52 +0900

    Add hello.py for diagnostics

---
+++ functions/hello.py
@@ -0,0 +1,8 @@
+import json
+
+def handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": { "Content-Type": "application/json" },
+        "body": json.dumps({ "message": "Hello from Netlify!" })
+    }
